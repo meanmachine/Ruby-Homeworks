f7ac93b411ed19be7c8cffd5ba070c30e4dce6de
meanmachine <metodi.st.dimitrov@gmail.com>
2014-01-20 16:11:21 +0200

    - No need of additional logic, using pred
    - Better variable naming used

--- HW1_Monkey_patching/solution.rb
+++ HW1_Monkey_patching/solution.rb
@@ -1,7 +1,7 @@
 class Integer
   def prime?
-    return false if pred < 1
-    2.upto(pred).all? { |integer| remainder(integer).nonzero? }
+    return false if self < 2
+    2.upto(pred).all? { |divisor| remainder(divisor).nonzero? }
   end
 
   def prime_factors
