6f9f4204d58a25e941e3f278c6f2de5cdd7bd9f6
meanmachine <metodi.st.dimitrov@gmail.com>
2014-01-20 15:41:05 +0200

    Initial homeworks, provided on Ruby course

---
+++ HW1_Monkey_patching/sk.bat
@@ -0,0 +1,2 @@
+skeptic --lines-per-method 8 --line-length 90 --max-nesting-depth 3 --methods-per-class 8 --no-semicolons --no-trailing-whitespace solution.rb
+
---
+++ HW2_To_Do_List/sk.bat
@@ -0,0 +1,1 @@
+skeptic --lines-per-method 4 --line-length 80 --max-nesting-depth 1 --methods-per-class 8 --no-semicolons --no-trailing-whitespace solution.rb
